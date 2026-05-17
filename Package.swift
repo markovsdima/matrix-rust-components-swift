8de4d0636f15f8c5ff2ccf16a9e8b69ca7879f17
markovsdima <113052998+markovsdima@users.noreply.github.com>
2026-05-17 16:51:29 +0300

    Bump to version zyna-ffi-26.05.13-direct-video-beta.1

--- Package.swift
+++ Package.swift
@@ -1,8 +1,8 @@
 // swift-tools-version:5.7
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 import PackageDescription
-let checksum = "59464c3148ddb0d7ff28ebc4c8c837380c69985763742dc01e2707388cfbb442"
-let version = "zyna-ffi-26.05.13-direct-redact-beta.1"
+let checksum = "27569b0f1e7f0c005507a8f0e650dc0f744ec075b4248b1267865f4aa9c992e6"
+let version = "zyna-ffi-26.05.13-direct-video-beta.1"
 let url = "https://github.com/markovsdima/matrix-rust-sdk/releases/download/\(version)/MatrixSDKFFI.xcframework.zip"
 let package = Package(
     name: "MatrixRustSDK",
