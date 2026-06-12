05bfca9735813bee29298c7160daa4e7608a418f
markovsdima <113052998+markovsdima@users.noreply.github.com>
2026-06-12 14:18:00 +0300

    Update MatrixRustSDK for read receipt summaries FFI
    Bump MatrixSDKFFI to zyna-ffi-26.05.13-latest-own-read-receipts-beta.1.
    
    Regenerate Swift bindings for read receipt summary support:
    - Room.latestOwnMainTimelineReadReceiptSummary()
    - Room.readReceiptSummaryForEvent(eventId:scope)
    - EventReadReceiptSummary
    - ReadReceiptThreadScope

--- Package.swift
+++ Package.swift
@@ -1,8 +1,8 @@
 // swift-tools-version:5.7
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 import PackageDescription
-let checksum = "b82bf94b1024a4c4d535a8298fa9753280eb0a4d536ebd32da5c975cf452af2a"
-let version = "zyna-ffi-26.05.13-matrixrtc-reactions-beta.1"
+let checksum = "f9e2a70a4600283ef78744549023d6e62769b44423894e04123c5c95a8ec696a"
+let version = "zyna-ffi-26.05.13-latest-own-read-receipts-beta.1"
 let url = "https://github.com/markovsdima/matrix-rust-sdk/releases/download/\(version)/MatrixSDKFFI.xcframework.zip"
 let package = Package(
     name: "MatrixRustSDK",
